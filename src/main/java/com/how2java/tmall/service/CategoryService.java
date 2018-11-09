374458494a0379b424bf47553012385000e30466
wulening <wulening@foxmail.com>
2018-11-09 11:46:18 +0800

    Service

---
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.how2java.tmall.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.how2java.tmall.dao.CategoryDAO;
+import com.how2java.tmall.pojo.Category;
+
+@Service //标志这个类为service类
+//创建一个sort对象，然后通过id排序，然后用DAO查询
+public class CategoryService {
+    @Autowired CategoryDAO categoryDAO; //自动装配上个DAO对象的
+    public List<Category> list(){
+        Sort sort = new Sort(Sort.Direction.DESC,"id");
+        return categoryDAO.findAll(sort);
+    }
+}
